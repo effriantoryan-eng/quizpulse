2ddef025d5e2962fcd8782de4038abc368ea1521
Ryan Effrianto <effrianto.ryan@gmail.com>
2026-05-25 13:03:21 +1000

    add Microsoft Entra authentication - teacher routes protected

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useIsAuthenticated } from '@azure/msal-react'
 import Login from './pages/Login'
 import CreateQuestion from './pages/teacher/CreateQuestion'
 import QuestionBank from './pages/teacher/QuestionBank'
@@ -7,15 +8,22 @@ import SendQuiz from './pages/teacher/SendQuiz'
 import TakeQuiz from './pages/student/TakeQuiz'
 import Completion from './pages/student/Completion'
 
+function ProtectedRoute({ children }) {
+  const isAuthenticated = useIsAuthenticated()
+  return isAuthenticated ? children : <Navigate to="/" />
+}
+
 function App() {
+  const isAuthenticated = useIsAuthenticated()
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/teacher/create" element={<CreateQuestion />} />
-        <Route path="/teacher/bank" element={<QuestionBank />} />
-        <Route path="/teacher/build" element={<BuildQuiz />} />
-        <Route path="/teacher/send" element={<SendQuiz />} />
+        <Route path="/" element={isAuthenticated ? <Navigate to="/teacher/create" /> : <Login />} />
+        <Route path="/teacher/create" element={<ProtectedRoute><CreateQuestion /></ProtectedRoute>} />
+        <Route path="/teacher/bank" element={<ProtectedRoute><QuestionBank /></ProtectedRoute>} />
+        <Route path="/teacher/build" element={<ProtectedRoute><BuildQuiz /></ProtectedRoute>} />
+        <Route path="/teacher/send" element={<ProtectedRoute><SendQuiz /></ProtectedRoute>} />
         <Route path="/student/quiz/:id" element={<TakeQuiz />} />
         <Route path="/student/done" element={<Completion />} />
       </Routes>
